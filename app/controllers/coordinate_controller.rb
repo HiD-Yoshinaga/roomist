515525259a1062ea6d8f20d892904d89da08ec8e
HiD-Yoshinaga <70322682+HiD-Yoshinaga@users.noreply.github.com>
2020-10-25 23:59:19 +0900

    Update coordinate_controller.rb

--- app/controllers/coordinate_controller.rb
+++ app/controllers/coordinate_controller.rb
@@ -4,4 +4,16 @@ class CoordinateController < ApplicationController
     @coordinate = Coordinate.all.order('created_at DESC')
   end
 
+  def new
+    @coordinate = Coordinate.new
+  end
+
+  def create
+    @coordinate = Coordinate.new(item_params)
+    if @coordinate.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 end
